46d7fe3dee3b812d26115ede1d4071ed1d83bb6d
Anna Chirkova <anna.davainis@gmail.com>
2022-08-09 14:34:53 +0500

    example005 if and else:не получилось

---
+++ example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+
+if (username =="Маша");
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+}
