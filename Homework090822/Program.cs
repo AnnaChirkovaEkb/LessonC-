7550d23552056c0d0eff5ddc49ced650474c1a07
Anna Chirkova <anna.davainis@gmail.com>
2022-08-10 22:46:31 +0500

    Do 1 task of homework 090822 about seach  second figure in number

---
+++ Homework090822/Program.cs
@@ -0,0 +1,14 @@
+﻿/*// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Input number which have 3 figure:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int ed=num%10;
+int sot=num/100;
+
+int result=(num-sot*100-ed)/10;
+
+Console.Write ($"The  second figure of number is: {result} ");
+Console.WriteLine();
+
+*/
