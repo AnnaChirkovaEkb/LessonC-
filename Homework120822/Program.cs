5fc711a99a86c213025333aadde85d9f63b04d11
Anna Chirkova <anna.davainis@gmail.com>
2022-08-13 12:57:47 +0500

    Do 1 task about pantoram

--- Homework120822/Program.cs
+++ Homework120822/Program.cs
@@ -1,4 +1,43 @@
-﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+﻿/*
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.(зеркальное)
+
+int Palindrom (int n)
+{
+    int tenthous=n/10000;
+    //Console.WriteLine(tenthous);
+    int thous=(n/1000)%10;
+    //Console.WriteLine(thous);
+    int doz=(n%100)/10;
+    //Console.WriteLine(doz);
+    int ed=n%10;
+    //Console.WriteLine(ed);  
+    
+    if(n<10000 && n>99999)
+        Console.WriteLine("Error!");
+    else if(tenthous==ed && thous==doz)
+        
+        Console.WriteLine("Yes, it is a palindrom.");
+
+    else
+       Console.WriteLine("No, it is not a palindrom."); 
+
+return n;
+}
+
+Console.WriteLine("Input any whole number whitch have 5 figures:  ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+Palindrom(number);
+*/
+//Задача 21:Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double FindDistant(double  x1,double  y1,double  z1,double  x2,double  y2,double  z2)
+{
+    
+}
+
+/*
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 
 void ShowCube (int n)
@@ -20,3 +59,7 @@ int number=Convert.ToInt32(Console.ReadLine());
 ShowCube(number);
 
 Console.WriteLine();
+
+*/
+
+
