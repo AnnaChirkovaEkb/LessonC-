76ede66a60d302eb3a8483b114c232fbf2582441
Anna Chirkova <anna.davainis@gmail.com>
2022-08-13 12:02:56 +0500

    Do 3 task about cube all numbers to size

---
+++ Homework120822/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+void ShowCube (int n)
+{
+    Console.WriteLine($"Cubes of number {n} is: ");
+    int current=1;
+    while(current<=n)
+    {
+        int Cube=current*current*current;
+        Console.Write($"{Cube},");
+        current++;
+    }
+
+}
+
+Console.WriteLine("Input any whole number:  ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+ShowCube(number);
+
+Console.WriteLine();
