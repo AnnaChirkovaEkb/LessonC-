fcb56edb1ad3ea57782bf1063b9a6ed9c9d9f5b0
Anna Chirkova <anna.davainis@gmail.com>
2022-08-13 13:11:59 +0500

    Do 2 task about distance beetwen point in 3D.

--- Homework120822/Program.cs
+++ Homework120822/Program.cs
@@ -33,8 +33,29 @@ Palindrom(number);
 
 double FindDistant(double  x1,double  y1,double  z1,double  x2,double  y2,double  z2)
 {
-    
+    return Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
 }
+
+Console.WriteLine("Input x1:  ");
+double xA=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input y1:  ");
+double yA=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input z1:  ");
+double zA=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input x2:  ");
+double xB=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input y2:  ");
+double yB=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input z2:  ");
+double zB=Convert.ToDouble(Console.ReadLine());
+
+double coordinate=FindDistant(xA,yA,zA,xB,yB,zB);
+Console.WriteLine($"Distance is {coordinate}.");
 
 /*
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
